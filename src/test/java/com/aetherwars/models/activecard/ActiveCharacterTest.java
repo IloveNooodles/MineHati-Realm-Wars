c21c87f26122c39e9cfa64a7bf823c985a20afac
IloveNooodles <mgarebaldhie80@gmail.com>
2022-04-27 01:47:15 +0700

    adding attack overworld test

--- src/test/java/com/aetherwars/models/activecard/ActiveCharacterTest.java
+++ src/test/java/com/aetherwars/models/activecard/ActiveCharacterTest.java
@@ -5,8 +5,7 @@ import com.aetherwars.models.carddata.Character;
 import com.aetherwars.models.extras.Type;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotSame;
+import static junit.framework.TestCase.*;
 
 public class ActiveCharacterTest {
 
@@ -86,7 +85,7 @@ public class ActiveCharacterTest {
 
     @Test
     public void healthTest() {
-        Character pigmen = new Character("pigmen", "Bau", new Type(CharacterType.NETHER), "aaw", 5, 5, 1, 1, 1);
+        Character pigmen = new Character("pigmen", "Bau", new Type(CharacterType.NETHER), "aaw", 5, 5, 1, 2, 1);
         ActiveCharacter iqi = new ActiveCharacter(pigmen);
         iqi.levelUp();
         assertEquals((int) iqi.getHp(), 6);
@@ -95,16 +94,55 @@ public class ActiveCharacterTest {
         assertEquals((int) iqi.getHp(), 7);
 
         iqi.levelDown();
-        assertNotSame((int) iqi.getHp(), 7);
-        assertNotSame((int) iqi.getMaxHp(), 7);
+        assertEquals((int) iqi.getHp(), 6);
+        assertEquals((int) iqi.getMaxHp(), 6);
 
         iqi.swap();
-        assertNotSame((int) iqi.getHp(), 5);
-        assertNotSame((int) iqi.getMaxHp(), 5);
+        assertNotSame((int) iqi.getMaxHp(), 6);
+        assertNotSame((int) iqi.getHp(), 7);
+        assertEquals((int) iqi.getMaxHp(), 7);
 
         iqi.increaseStats(0, 10);
-        assertNotSame((int) iqi.getHp(), 15);
-        assertEquals((int) iqi.getMaxHp(), 15);
+        assertNotSame((int) iqi.getHp(), 17);
+        assertEquals((int) iqi.getMaxHp(), 17);
+    }
+
+    @Test
+    public void attackOverworldTest() {
+        Character zombie = new Character("zombie", "Pedang", new Type(CharacterType.OVERWORLD), "-", 5, 5, 1, 2, 3);
+        Character skeleteon = new Character("skeleton", "Panah", new Type(CharacterType.OVERWORLD), "-", 6, 6, 1, 1, 1);
+        Character pigmen = new Character("pigmen", "Bau", new Type(CharacterType.NETHER), "-", 7, 10, 3, 3, 4);
+        Character endermen = new Character("endermen", "Bau", new Type(CharacterType.END), "-", 8, 15, 5, 5, 5);
+        ActiveCharacter Zombie = new ActiveCharacter(zombie);
+        ActiveCharacter Skeleton = new ActiveCharacter(skeleteon);
+        ActiveCharacter Pigmen = new ActiveCharacter(pigmen);
+        ActiveCharacter Endermen = new ActiveCharacter(endermen);
+
+        assertEquals((int) Zombie.getAtk(), 5);
+        assertEquals((int) Pigmen.getAtk(), 7);
+        assertEquals((int) Endermen.getAtk(), 8);
+
+        Zombie.addExp(1);
+        assertNotSame((int) Zombie.getAtk(), 5);
+        assertEquals((int) Zombie.getAtk(), 7);
+
+        Zombie.attack(Skeleton);
+        assertNotSame(Skeleton.getHp(), 6);
+        assertTrue(Skeleton.getHp() == 0.0);
+
+        Zombie.levelUp();
+        Zombie.levelUp();
+        Zombie.levelDown();
+        assertNotSame((int) Zombie.getAtk(), 8);
+        assertEquals((int) Zombie.getAtk(), 9);
+
+        Zombie.increaseStats(-5, 0);
+        assertEquals((int) Zombie.getAtk(), 4);
+    }
+
+    @Test
+    public void attackNetherTest() {
+
     }
 }
 
