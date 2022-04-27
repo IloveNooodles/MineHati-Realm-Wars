a569eeae12d68d4316353864aae615ec2802a270
Aira Thalca <13520101@std.stei.itb.ac.id>
2022-04-27 15:55:13 +0700

    add test activate for unit test hand

--- src/test/java/com/aetherwars/models/cardcontainer/HandTest.java
+++ src/test/java/com/aetherwars/models/cardcontainer/HandTest.java
@@ -1,12 +1,17 @@
 package com.aetherwars.models.cardcontainer;
 
 import com.aetherwars.enums.CharacterType;
+import com.aetherwars.models.carddata.spell.Spell;
 import com.aetherwars.models.extras.Type;
+import com.aetherwars.models.game.Game;
 import com.aetherwars.models.carddata.Character;
+import com.aetherwars.models.carddata.spell.PTN;
+
 import org.junit.Test;
 
 import static junit.framework.TestCase.*;
 
+import com.aetherwars.models.activecard.ActiveCharacter;
 import com.aetherwars.models.card.Card;
 
 public class HandTest {
@@ -74,6 +79,31 @@ public class HandTest {
             else {
                 assert false;
             }
+        }
+    }
+
+    @Test
+    public void TestActivate() {
+        try {
+            Character pigment = new Character("pigment", "Bau", new Type(CharacterType.NETHER), "aaw", 5, 5, 1, 1, 1);
+            Spell PTN = new PTN("UP", "-", "-", 999, 999, 0, 1);
+            Card Pigment = new Card(pigment);
+            Card potion = new Card(PTN);
+
+            Hand hand = new Hand();
+            Board board = Game.getInstance().getPlayerBoard();
+            hand.add(Pigment);
+            hand.add(potion);
+
+            hand.activate(0, 0);
+            assertEquals(board.get(0).getName(), pigment.getName());
+
+            hand.activate(0, 0);
+            assertEquals(board.get(0).getAtk(), 1004.0);
+            assertEquals(board.get(0).getMaxHp(), 1004.0);
+        }
+        catch (Exception e) {
+            assert false;
         }
     }
 }
