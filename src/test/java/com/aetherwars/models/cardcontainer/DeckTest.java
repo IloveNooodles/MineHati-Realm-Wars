47ee25313eddb325d560290e2cfdcd472403f8fd
Aira Thalca <13520101@std.stei.itb.ac.id>
2022-04-27 12:57:58 +0700

    add unit test board

--- src/test/java/com/aetherwars/models/cardcontainer/DeckTest.java
+++ src/test/java/com/aetherwars/models/cardcontainer/DeckTest.java
@@ -1,6 +1,8 @@
 package com.aetherwars.models.cardcontainer;
 
 import com.aetherwars.enums.CharacterType;
+import com.aetherwars.exception.EmptyDeckException;
+import com.aetherwars.exception.EmptySlotException;
 import com.aetherwars.models.card.Card;
 import com.aetherwars.models.extras.Type;
 import com.aetherwars.models.carddata.Character;
@@ -14,15 +16,12 @@ public class DeckTest {
     public void TestAddDeck() {
         Deck deck = new Deck();
         Character pigment = new Character("pigment", "Bau", new Type(CharacterType.NETHER), "aaw", 5, 5, 1, 1, 1);
-        Character zombie = new Character("zombie", "Pedang", new Type(CharacterType.OVERWORLD), "-", 5, 5, 1, 2, 3);
-        Card Zombie = new Card(zombie);
         Card Pigment = new Card(pigment);
         deck.add(Pigment);
-        deck.add(Zombie);
         deck.shuffle();
-        assertEquals(deck.getCards().get(0), deck.get(0));
-        System.out.println(deck.toString());
-        assertTrue(deck.toString().equals("Deck: 2"));
+        assertEquals(deck.getCards().get(0).getCost(), Pigment.getCost());
+        assertTrue(deck.getCards().get(0).getName().equals(Pigment.getName()));
+        assertTrue(deck.toString().equals("Deck: 1"));
     }
 
     @Test
@@ -37,10 +36,45 @@ public class DeckTest {
             deck.add(Skeleton);
             deck.remove(0);
             assertEquals(deck.get(0), Skeleton);
-            deck.remove(1);
+            assertEquals(deck.getCards().size(), 1);
         }
         catch (Exception e) {
-            assert true;
+            assert false;
+        }
+    }
+
+    @Test
+    public void TestEmptyDeck() {
+        try {
+            Deck deck = new Deck();
+            deck.remove(0);
+        }
+        catch (Exception e) {
+            if (e.getMessage().equals("Deck is empty!")) {
+                assert true;
+            }
+            else {
+                assert false;
+            }
+        }
+    }
+
+    @Test
+    public void TestEmptySlot() {
+        try {
+            Deck deck = new Deck();
+            Character pigment = new Character("pigment", "Bau", new Type(CharacterType.NETHER), "aaw", 5, 5, 1, 1, 1);
+            Card Pigment = new Card(pigment);
+            deck.add(Pigment);
+            deck.remove(2);
+        }
+        catch (Exception e) {
+            if (e.getMessage().equals("Slot is empty!")) {
+                assert true;
+            }
+            else {
+                assert false;
+            }
         }
     }
 }
